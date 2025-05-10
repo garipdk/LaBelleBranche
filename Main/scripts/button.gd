1368622f28053afa80a72224b0f72599fe372329
EnterN1me <mickael.guerchon@gmail.com>
2025-05-10 16:41:29 +0200

    button_shape

--- Main/scripts/button.gd
+++ Main/scripts/button.gd
@@ -2,16 +2,28 @@ extends StaticBody3D
 
 @onready var label_3d: Label3D = $Label3D
 @onready var sprite_3d: Sprite3D = $Sprite3D
+@onready var collision_shape_3d: CollisionShape3D = $CollisionShape3D
 
 @export_category("Main Button")
-@export var text : String = "default text"
+@export var text: String = "default text"
 @export var texture: Texture2D = preload("res://icon.svg")
+@export var x_scale: int = 3
+
+var texture_height: int
+var texture_width: int
+var width_on_height_ratio: float
 
 signal turn
 
 func _ready() -> void:
 	label_3d.text = text
 	sprite_3d.texture = texture
+	self.texture_height = texture.get_height()
+	self.texture_width = texture.get_width()
+	self.width_on_height_ratio  = self.texture_width/self.texture_height
+	sprite_3d.scale = Vector3(x_scale*width_on_height_ratio,1,1)
+	collision_shape_3d.shape.size = Vector3(x_scale*width_on_height_ratio,1,1)
+	
 	
 func _on_input_event(
 	camera: Node,
