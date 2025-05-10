6e26f08b3da9ac5388d522a61eb90a76745e2ff3
nathanBarloy <nbarloy@gmail.com>
2025-05-10 09:49:45 +0200

    Merge

--- Collection/collection.gd
+++ Collection/collection.gd
@@ -2,6 +2,14 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var pot_dict = {
+		"emotion": $PotList/PotEmotion,
+		"rationalite": $PotList/PotRationalite,
+		"multifruit": $PotList/PotMultifruit,
+	}
 	var save = Save.load_save()
-	print(save)
+	for tree_name in save:
+		pot_dict[tree_name].set_tree_visibility(save[tree_name])
+		
+	
 	
